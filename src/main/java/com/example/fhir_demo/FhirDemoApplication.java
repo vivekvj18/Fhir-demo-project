e814a92c84bfe68f5f1c8a35f799281fbdca7399
vivek joshi <2001vivekjoshi@gmail.com>
2026-01-31 14:41:00 +0530

    Dependency added

---
+++ src/main/java/com/example/fhir_demo/FhirDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.fhir_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FhirDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FhirDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fhir_demo/FhirDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fhir_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FhirDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
